8d3f73852a3a71145e55bdcb0e38ce0fb21fd922
Marco <marcojimenez1996@hotmail.com>
2025-02-15 21:00:25 +0100

    Added SASS and little example of show list of users

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Marcodona</title>
+  <link href="../resources/sass/principal.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <h1>Marcodona</h1>
+    <nav>
+      <a href="{{ route('shop') }}">Tienda</a>
+      <a href="">Pedidos</a>
+    </nav>
+  </header>
+
+  <main>
+
+    @forelse($users as $user)
+      <li>{{ $user->name }}</li>
+    @empty
+      <li>No hay usuarios registrados.</li>
+    @endforelse
+
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [UserController::class, 'index']);
+
+Route::get('/shop', function () {
+    return "Tienda";
+})->name('shop');
