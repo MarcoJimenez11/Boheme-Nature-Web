217d1d4f6f1ad8948e0dff981ddd37d059d8a447
Marco <marcojimenez1996@hotmail.com>
2025-03-15 12:53:34 +0100

    Removed scss, added Tailwind config and currently started new header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+/**
+ * Menú desplegable de Header al pulsar el botón
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownButton = document.getElementById('mega-menu-dropdown-button');
+    const dropdownMenu = document.getElementById('mega-menu-dropdown');
+
+    dropdownButton.addEventListener('click', function () {
+        dropdownMenu.classList.toggle('hidden');
+    });
+});
