6e3aa02cf52a494b19813a0feb8ba4e7edc60516
Marco <marcojimenez1996@hotmail.com>
2025-06-13 13:07:14 +0200

    testing again 2

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marcodona</title>
-    <link href="{{ asset('../resources/sass/principal.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @vite('resources/js/categoriesMenu.js')
 </head>
 
